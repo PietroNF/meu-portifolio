b303e153251a468996d5bccb24bc331d0fe9b4d2
pietro Nuernberg Fernandes <115109378+PietroNF@users.noreply.github.com>
2024-08-23 19:13:02 -0300

    *

--- js/mobile-navbar.js
+++ js/mobile-navbar.js
@@ -1,15 +1,15 @@
 
 
+
 class MobileNavbar {
-  constructor(mobileMenu, navList, navLinks ) {
+  constructor(mobileMenu, navList, navLinks) {
     this.mobileMenu = document.querySelector(mobileMenu);
     this.navList = document.querySelector(navList);
     this.navLinks = document.querySelectorAll(navLinks);
     this.activeClass = "active";
+
     this.handleClick = this.handleClick.bind(this);
- 
   }
-  
 
   animateLinks() {
     this.navLinks.forEach((link, index) => {
@@ -23,7 +23,6 @@ class MobileNavbar {
 
   handleClick() {
     this.navList.classList.toggle(this.activeClass);
-    
     this.mobileMenu.classList.toggle(this.activeClass);
     this.animateLinks();
   }
@@ -32,13 +31,10 @@ class MobileNavbar {
     this.mobileMenu.addEventListener("click", this.handleClick);
   }
 
-
   init() {
     if (this.mobileMenu) {
       this.addClickEvent();
-    
     }
-    
     return this;
   }
 }
@@ -48,13 +44,8 @@ const mobileNavbar = new MobileNavbar(
   ".nav-list",
   ".nav-list li",
 );
-
-
-
-
-
-
-
-
 mobileNavbar.init();
+
+
+
 
